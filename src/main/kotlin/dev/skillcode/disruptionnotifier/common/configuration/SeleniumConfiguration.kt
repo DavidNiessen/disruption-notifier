1a05b41fa912d2ebfa0c2e0cf79dd3527c21f36b
David Niessen <68539499+DavidNiessen@users.noreply.github.com>
2025-04-06 00:49:06 +0200

    add docker-compose.yml

--- src/main/kotlin/dev/skillcode/disruptionnotifier/common/configuration/SeleniumConfiguration.kt
+++ src/main/kotlin/dev/skillcode/disruptionnotifier/common/configuration/SeleniumConfiguration.kt
@@ -1,25 +1,29 @@
 package dev.skillcode.disruptionnotifier.common.configuration
 
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Scope
+import java.net.URL
 
 @Configuration
-class SeleniumConfiguration {
+class SeleniumConfiguration(
+    @Value("\${remote.driver.url}") private val seleniumUrl: String,
+) {
 
     @Bean(destroyMethod = "quit")
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     fun webDriver(): WebDriver {
         val options = ChromeOptions().apply {
             // TODO allow disabling headless mode for debugging
-            addArguments("--headless")
+            addArguments("--headless=new")
         }
         // TODO allow to set browser type in config
-        val driver = ChromeDriver(options)
+        val driver = RemoteWebDriver(URL(seleniumUrl), options)
         return driver
     }
 }
